fdd34abb5b893a6c4602ff2a97ecf9ba26ff7aac
Daniyal Ahmed <daniyalahmed520@gmail.com>
2021-09-14 16:05:08 +0500

    Setup the initial architecture

--- app/src/main/java/com/daniyal/payment/ui/BaseActivity.java
+++ app/src/main/java/com/daniyal/payment/ui/BaseActivity.java
@@ -27,6 +27,7 @@ public abstract class BaseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         ButterKnife.bind(this);
     }
 
